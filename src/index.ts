41d13b089469139666297c96100cae2ca7a2b50e
ben3101 <111755107+ben3101@users.noreply.github.com>
2023-11-28 13:09:32 +0000

    begin adding vc files

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ export * from './authentication/PublicCredentialsExtractor';
 export * from './authentication/UnionCredentialsExtractor';
 export * from './authentication/UnsecureConstantCredentialsExtractor';
 export * from './authentication/UnsecureWebIdExtractor';
+export * from './authentication/VcExtractor';
 
 // Authorization/Access
 export * from './authorization/access/AccessChecker';
